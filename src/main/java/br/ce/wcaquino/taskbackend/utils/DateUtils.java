b7ab0cbd87ce0a33725efd2964d9997f925266c1
LuisVanique <cunhagustavo142@gmail.com>
2024-12-06 23:35:57 -0300

    more test

--- src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
+++ src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
@@ -4,6 +4,10 @@ import java.time.LocalDate;
 
 public class DateUtils {
 
+	private DateUtils() {
+		
+	}
+	
 	public static boolean isEqualOrFutureDate(LocalDate date) {
 		return date.isEqual(LocalDate.now()) || date.isAfter(LocalDate.now());
 	}
